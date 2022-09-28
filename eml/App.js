09406cc33897b2826a8c7bf23cdcd2847b0a8821
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2022-09-28 13:32:45 +0200

    Clean app.js

--- eml/App.js
+++ eml/App.js
@@ -1,42 +1,21 @@
-import React from 'react';
-import { StyleSheet, } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, View } from 'react-native';
 
-import {
-  RecoilRoot,
-} from 'recoil';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from './screens/Home/Home';
-import SearchScreen from './screens/Search/Search';
-import LibraryScreen from './screens/Library/Library';
-import CourseScreen from './screens/Course/Course';
-import SectionScreen from './screens/Section/Section';
-import Login from './screens/Login/Login';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ animation: 'none', headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen}/>
-          <Stack.Screen name="Search" component={SearchScreen}/>
-          <Stack.Screen name="Library" component={LibraryScreen}/>
-          <Stack.Screen name="Course" component={CourseScreen}/>
-          <Stack.Screen name="Section" component={SectionScreen}/>
-          <Stack.Screen name="Login" component={Login}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </RecoilRoot>
+    <View style={styles.container}>
+        <Text>Hej</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0F1F0',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
