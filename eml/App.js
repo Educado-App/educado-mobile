09170c70c87839ab77c2d67690bcdad9de1b0336
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2022-09-29 15:09:52 +0200

    More Fixes to dependencies and a template for button

--- eml/App.js
+++ eml/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View } from 'react-native';
-
+import {StyleSheet, Text, View, Button} from 'react-native';
+import VeryNiceButton from './components/VeryNiceButton';
+// import FontAwesomeIcon from '@fortawesome/fontawesome-free';
+// import {faShapes} from '@fortawesome/free-solid-svg-icons/faShapes';
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text>Hej</Text>
+        <Text>This is ap js</Text>
+        <View>
+        {/* <FontAwesomeIcon icon={faShapes}/> */}
+        <VeryNiceButton />
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+
+  }
 });
