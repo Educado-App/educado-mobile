8925f3d8988a67ea4aaffb34ecd2aec1f06e4348
Hijabnasir21 <hijabnasir26@gmail.com>
2022-09-30 11:27:58 +0200

    This was added

--- eml/App.js
+++ eml/App.js
@@ -7,7 +7,7 @@ import {faShapes} from '@fortawesome/free-solid-svg-icons/';
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text>This is app js test</Text>
+        <Text>This is app js</Text>
         <View>
         <FontAwesomeIcon icon={faShapes}/>
         <VeryNiceButton></VeryNiceButton>
