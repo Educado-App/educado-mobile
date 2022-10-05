32430967b1c460b3acf15639b2b3239e1794f656
Mandrupnicolai <nicolai.uhre.com@gmail.com>
2022-10-05 14:08:12 +0200

    Made screen for wronganswer

--- eml/App.js
+++ eml/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 
+// import WrongAnswerComponent from './screens/Session/WrongAnswerScreen';
 import { StyleSheet, Text, View } from 'react-native';
 import SessionComponent from './screens/Session/SessionScreen';
 export default function App() {
   return (
+    // <WrongAnswerComponent/> 
     <SessionComponent/>
   );
 }
---
+++ eml/screens/Session/WrongAnswerScreen.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+
+import React from 'react';
+
+import { StyleSheet, Text, View, ViewComponent } from 'react-native';
+import LearningInputVideo from '../../components/sessions/video/LearningInputVideoExample1';
+import LeaveButton from '../../components/sessions/LeaveButton';
+import CustomProgressBar from '../../components/sessions/Progressbar';
+import { Icon } from "@rneui/themed";
+
+export default function WrongAnswerComponent()  {
+    return (
+        <View style={styles.container}>
+            <View style={{ flex: 0.5 }}>
+                <View style={[styles.row, { paddingTop: '15%', paddingRight:'7%'}]}>
+                <View style={{}}>
+                    <LeaveButton></LeaveButton>
+                </View>
+                <View style={{paddingTop: '6%'}}>
+                    <CustomProgressBar></CustomProgressBar>
+                </View>
+                </View>
+            </View>
+            <View style={{ flex: 2, width: '100%' }}>
+                <LearningInputVideo></LearningInputVideo>
+            </View>
+            <View style={{top: '5%', flex: 0.7}}>
+                <View
+                style={[
+                    styles.nextArrow,
+                    styles.buttonShadow,
+                    { shadowColor: "#2db300" },
+                ]}
+                >
+                <Icon
+                    size={70}
+                    name="chevron-right"
+                    type="material-community"
+                    color="white"
+                    onPress={() => checkChoice(choice)}
+                />
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+    },        
+    button: {
+        borderRadius: 15,
+        justifyContent: "center",
+        width: 140,
+        height: 110,
+    },
+    buttonShadow: {
+        shadowOpacity: 0.6,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 0 },
+    },
+    nextArrow: {
+        borderRadius: 15,
+        backgroundColor: "#2db300",
+        width: 300,
+        height: 75,
+        position: "relative",
+    },
+    paddingButtons: {
+        padding: 10,
+    },
+  });
+  
