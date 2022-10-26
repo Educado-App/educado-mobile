1845989f7c7c5b8ecbdf7261cfffba78fdd8b91f
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2022-10-26 14:10:33 +0200

    small changes to Explore

--- eml/screens/explore/Explore.jsx
+++ eml/screens/explore/Explore.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function Explore() {
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, alignItems: 'center', marginTop: 50, backgroundColor: 'blue' }}>
-                <Text style={{ fontSize: 30 }}> Some Title Text </Text>
+            <View style={{ flex: 1, alignItems: 'center', marginTop: 50 }}>
+                <Text style={{ fontSize: 30 }}> Explorar Novos Cursos </Text>
             </View>
-            <View style={{ flex: 1, backgroundColor: 'green', alignItems: 'center' }}>
-                <Text style={{ fontSize: 30 }}> Filtering Options </Text>
+            <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row' }}>
+                <TouchableOpacity>
+                    <Text style={{ fontSize: 20, paddingLeft: 20 }}> All</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={{ fontSize: 20, paddingLeft: 20 }}> Finance </Text>
+                </TouchableOpacity>
             </View>
             <View style={{ flex: 5, backgroundColor: 'red', alignItems: 'center' }}>
                 <Text style={{ fontSize: 30 }}> Courses </Text>
