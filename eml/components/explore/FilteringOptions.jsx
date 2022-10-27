2bfa5b4b26a7cddd630c938694e981783a25c266
Lajek20 <71812123+Lajek20@users.noreply.github.com>
2022-10-27 10:17:06 +0200

    explore filtering

--- eml/components/explore/FilteringOptions.jsx
+++ eml/components/explore/FilteringOptions.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-navigation'
+
+
+
 export default function FilteringOptions() {
+
     return (
         <View style={{ flexDirection: 'row' }}>
             <TouchableOpacity>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>All</Text>
+                <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} All </Text>
             </TouchableOpacity>
             <TouchableOpacity>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>Finance</Text>
+                <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} Finance  </Text>
             </TouchableOpacity>
             <TouchableOpacity>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>Health</Text>
+                <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} Health</Text>
             </TouchableOpacity>
         </View>
-    )
+    );
 }
+
