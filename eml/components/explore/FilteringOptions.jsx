3d110273860ff19ccf13c31a6822e86e73a004c8
Beniamin Lobodziec <thetroleklolekpl@gmail.com>
2022-10-27 10:33:54 +0200

    Merge

--- eml/components/explore/FilteringOptions.jsx
+++ eml/components/explore/FilteringOptions.jsx
@@ -1,40 +1,18 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import { View, Text, FlatList } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { SafeAreaView } from 'react-navigation'
-
-
-
-=======
 import React from 'react'
 import { View, Text, Pressable } from 'react-native'
->>>>>>> cbf347d4edf60667ea4c06289f562ef04b38e73b
 export default function FilteringOptions() {
 
     return (
         <View style={{ flexDirection: 'row' }}>
-<<<<<<< HEAD
-            <TouchableOpacity>
+            <Pressable>
                 <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} All </Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
+            </Pressable>
+            <Pressable>
                 <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} Finance  </Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
+            </Pressable>
+            <Pressable>
                 <Text style={{ fontSize: 24, paddingLeft: 10 }}>{'\u29BF'} Health</Text>
-            </TouchableOpacity>
-=======
-            <Pressable>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>All</Text>
             </Pressable>
-            <Pressable>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>Finance</Text>
-            </Pressable>
-            <Pressable>
-                <Text style={{ fontSize: 30, paddingLeft: 30 }}>Health</Text>
-            </Pressable>
->>>>>>> cbf347d4edf60667ea4c06289f562ef04b38e73b
         </View>
     );
 }
