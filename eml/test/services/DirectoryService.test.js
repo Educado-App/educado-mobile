7a03da29448a0c4d33329ffbc3492ad835fc7c71
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2022-11-14 11:43:11 +0100

    Added --watch to jest

--- eml/test/services/DirectoryService.test.js
+++ eml/test/services/DirectoryService.test.js
@@ -1,5 +1,6 @@
-import { CreateDirectory } from "../../services/DirectoryService";
+import { CreateDirectory, DeleteDirectory, ReadDirectory } from "../../services/DirectoryService";
 import * as FileSystem from 'expo-file-system';
+import { getInfoAsync } from "expo-file-system";
 
 // it('console.log "Created directory: Test"', () => {
 //     //CreateDirectory = jest.fn();
@@ -9,16 +10,27 @@ import * as FileSystem from 'expo-file-system';
 //   });
 
 it('Should create a directory with name "Test"', async () => {
-    //CreateDirectory('Test')
-    const p = FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + "Test")
 
-    p.then(value => {
-        console.log(value);
+    //CreateDirectory('Test')
+    // const p = FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + "Test")
+
+    // p.then(value => {
+    //     console.log(value);
+    // })
+    // .catch(err => {
+    //     console.log(err); // "Something went wrong"
+    // });
+
+    //DeleteDirectory("Test")
+    CreateDirectory("./Test")
+    const p = FileSystem.getInfoAsync(FileSystem.documentDirectory + "./Test2")
+    .then((exists) => {
+        console.log(exists)
     })
-    .catch(err => {
-        console.log(err); // "Something went wrong"
-    });
     
+    //ReadDirectory("")
+    //DeleteDirectory("84373yuheysyfges")
+    //console.log(FileSystem.getInfoAsync(FileSystem.documentDirectory + "Test"))
     
     //console.log(FileSystem.readDirectoryAsync(FileSystem.documentDirectory))
     //expect(FileSystem.readDirectoryAsync(FileSystem.documentDirectory + "Test")).toEqual
