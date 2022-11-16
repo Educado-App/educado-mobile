8a9dc5ca4abd24eac2460e9962b4eb4f37d8f438
tgrenaae <tgrena20@student.aau.dk>
2022-11-16 12:56:31 +0100

    added color theme to tailwind.config.js

--- eml/tailwind.config.js
+++ eml/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors:{
+      cyanBlue: "#65D4EE",
+      limeGreen: "#9DE89C",
+      yellow: "#FAC12F",
+      babyBlue: "#CFE9EF"
+    },
     extend: {
 
     },
