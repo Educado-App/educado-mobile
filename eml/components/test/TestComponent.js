eb1b4b52f0c31a0762d88e78e4ace8dd2ced5856
kojibeloved <lauritskoji@gmail.com>
2022-12-06 12:57:03 +0100

    new delete function

--- eml/components/test/TestComponent.js
+++ eml/components/test/TestComponent.js
@@ -93,6 +93,27 @@ export default function TestComponent() {
         }
     }
 
+    async function tryDeleteDirectory() {
+        const element = await StorageService.downloadCourse('635fb5b9b2fb6c4f49084682')
+
+        if (element == null) {
+            return null;
+        }
+
+        console.log(element);
+
+        const beforeDelete = AsyncStorage.getItem('635fb5b9b2fb6c4f49084682');
+        console.log(beforeDelete)
+
+        await StorageService.deleteElementFromLocalStorage(element.id);
+
+        const afterDelete = await AsyncStorage.getItem('635fb5b9b2fb6c4f49084682')
+
+        if (afterDelete != null) {
+            console.log(afterDelete);
+        }
+    }
+
     useEffect(() => {
         //test()
         //test3()
@@ -109,6 +130,7 @@ export default function TestComponent() {
         //CreateDirectory('test');
         //ReadDirectory('test');
         //DeleteDirectory('test');
+        tryDeleteDirectory();
     }
     );
 
