ca95c958cbf262d690c2766925a4e630c064bce7
MortenMunk <mortenmunk97@gmail.com>
2023-09-20 12:35:07 +0200

    Updated registerpage
    - logo and back button is now a component, and added to login and register
    - removed unused imports
    - updated hooks in registerform
    - added passwordGuidelines to FormTextField
    
    Co-Authored-By: Jack Skaarup Munn <92510884+JackSMunn@users.noreply.github.com>

--- eml/components/login/FormTextField.js
+++ eml/components/login/FormTextField.js
@@ -3,6 +3,19 @@ import React, { useState } from "react";
 import { Text, View, TextInput } from "react-native";
 
 export default function FormTextField(props) {
+
+  const displayPasswordGuidelines = (props) => {
+    if (props.passwordGuidelines) {
+      return (
+        <View className='flex flex-column mb-1'>
+          <Text className='ml-3 text-xs text-gray'>• Mínimo 8 caracteres</Text>
+          <Text className='ml-3 text-xs text-gray'>• Conter pelo menos uma letra</Text>
+        </View>
+      )
+    }
+  }
+
+
   return (
     <View className='mx-10'>
       <View className='flex flex-row'>
@@ -10,17 +23,18 @@ export default function FormTextField(props) {
         <Text className='ml-1 text-xs color-red-600'>{props.required ? '*' : ''}</Text>
       </View>
       <View className=''>
-        <TextInput className='h-50 border-solid border-2 br-25 py-1 pl-[10px] border-gray-200 rounded-xl'
+        <TextInput className='bg-white h-50 br-25 py-1 pl-[10px] border-gray-200 rounded-xl'
           //Phone Number
           placeholder={props.placeholder ? props.placeholder : ""}
           keyboardType={props.keyboardType ? props.keyboardType : "default"}
           autoComplete={props.autoComplete ? props.autoComplete : "off"}
           secureTextEntry={props.secureTextEntry ? props.secureTextEntry : false}
+          passwordGuidelines={props.passwordGuidelines ? props.passwordGuidelines : false}
         //onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
         //value={phoneNumber}
         />
       </View>
-      
+      {displayPasswordGuidelines(props)}
     </View>
 
   )
--- eml/screens/login/Login.js
+++ eml/screens/login/Login.js
@@ -5,6 +5,8 @@ import { useNavigation } from "@react-navigation/native";
 import LoginForm from "../../components/login/LoginForm";
 import FormButton from '../../components/login/FormButton';
 import LeaveButton from '../../components/exercise/LeaveButton';
+import LogoBackButton from '../../components/login/LogoBackButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const STORAGE_ID = '@local_id';
@@ -12,109 +14,96 @@ const STORAGE_PROGRESS = '@storage_progress';
 const LOGIN_TOKEN = '@loginToken';
 
 export default function Login(props) {
-  
-	const navigation = useNavigation();
 
-	// Check if local user id is set
-	// If not, then generate and save
-	// If yes, then continue
-	// Check if language and country is set
-	// If not, then prompt user and save
-	// If yes, then continue
+  const navigation = useNavigation();
 
-	const [localId, setLocalId] = useState(String(Date.now)); // Local state variable for storing local user id
-	// eslint-disable-next-line no-unused-vars
-	const [loginToken, setLoginToken] = useState('');
+  // Check if local user id is set
+  // If not, then generate and save
+  // If yes, then continue
+  // Check if language and country is set
+  // If not, then prompt user and save
+  // If yes, then continue
 
-	// Function for reading local user id from async local storage
-	// eslint-disable-next-line no-unused-vars
-	const readId = async () => {
-		try {
-			const fetchedLocalId = await AsyncStorage.getItem(STORAGE_ID);
+  const [localId, setLocalId] = useState(String(Date.now)); // Local state variable for storing local user id
+  // eslint-disable-next-line no-unused-vars
+  const [loginToken, setLoginToken] = useState('');
 
-			if (fetchedLocalId !== null) {
-				setLocalId(fetchedLocalId);
-				console.log('Already set, now logged in!');
-				const obj = {
-					activeCourses: [],
-					finishedCourses: [],
-					upNext: [],
-				};
+  // Function for reading local user id from async local storage
+  // eslint-disable-next-line no-unused-vars
+  const readId = async () => {
+    try {
+      const fetchedLocalId = await AsyncStorage.getItem(STORAGE_ID);
 
-				await AsyncStorage.setItem(STORAGE_PROGRESS, JSON.stringify(obj));
+      if (fetchedLocalId !== null) {
+        setLocalId(fetchedLocalId);
+        console.log('Already set, now logged in!');
+        const obj = {
+          activeCourses: [],
+          finishedCourses: [],
+          upNext: [],
+        };
 
-
-			} else {
-
-				try {
-					await AsyncStorage.setItem(STORAGE_ID, localId);
-
-					const obj = {
-						activeCourses: [],
-						finishedCourses: [],
-						upNext: [],
-					};
-
-					await AsyncStorage.setItem(STORAGE_PROGRESS, JSON.stringify(obj));
-
-					console.log('User successfully created and stored!');
-					navigation.navigate('Home');
-				} catch (error) {
-					console.log('Error when storing user...')
-				}
-			}
-
-		} catch (error) {
-			console.log('Failed to fetch the data from storage');
-		}
-	};
-
-	const checkLoginToken = async () => {
-
-		try {
-			const fetchedToken = await AsyncStorage.getItem(LOGIN_TOKEN);
-
-			if (fetchedToken !== null) {
-				setLoginToken(fetchedToken);
-				console.log('Already logged in!');
-				console.log('Token: ' + fetchedToken);
-				navigation.navigate('HomeStack');
-			}
-
-		} catch (error) {
-			console.log('Failed to fetch the login token from storage');
-		}
-
-	}
-
-	useEffect(() => {
-		// readId();
-		checkLoginToken();
-	}, []);
+        await AsyncStorage.setItem(STORAGE_PROGRESS, JSON.stringify(obj));
 
 
-	return (
-		<View className='mt-10'>
-			<View className='flex-row justify-center w-full mt-4'>
-        {/* TODO: Implement with general back button instead */}
-				<View className='absolute left-0'>
-					<LeaveButton
-						navigationPlace='Home'
-					/>
-				</View>
-				{/* Educado logo */}
-				<Image 
-          source={require('../../assets/logo_educado.png')}
-          className='h-12'
-          resizeMode='contain'
-         />
-			</View>
-			{/* Login form */}
+      } else {
+
+        try {
+          await AsyncStorage.setItem(STORAGE_ID, localId);
+
+          const obj = {
+            activeCourses: [],
+            finishedCourses: [],
+            upNext: [],
+          };
+
+          await AsyncStorage.setItem(STORAGE_PROGRESS, JSON.stringify(obj));
+
+          console.log('User successfully created and stored!');
+          navigation.navigate('Home');
+        } catch (error) {
+          console.log('Error when storing user...')
+        }
+      }
+
+    } catch (error) {
+      console.log('Failed to fetch the data from storage');
+    }
+  };
+
+  const checkLoginToken = async () => {
+
+    try {
+      const fetchedToken = await AsyncStorage.getItem(LOGIN_TOKEN);
+
+      if (fetchedToken !== null) {
+        setLoginToken(fetchedToken);
+        console.log('Already logged in!');
+        console.log('Token: ' + fetchedToken);
+        navigation.navigate('HomeStack');
+      }
+
+    } catch (error) {
+      console.log('Failed to fetch the login token from storage');
+    }
+
+  }
+
+  useEffect(() => {
+    // readId();
+    checkLoginToken();
+  }, []);
+
+
+  return (
+    <SafeAreaView className='mt-10'>
+      <LogoBackButton />
+      {/* Login form */}
       <View className='my-8'>
-			  <LoginForm/>
+        <LoginForm />
       </View>
-			{/* Register button */}
-			<FormButton label='Registrar uma nova conta' onPress={() => navigation.navigate('Register')}/>
-		</View>
-	);
+      {/* Register button */}
+      <FormButton label='Registrar uma nova conta' onPress={() => navigation.navigate('Register')} />
+    </SafeAreaView>
+  );
 }
