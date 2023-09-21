e936a19b4aec2a127d3e85fb54cef8738e6fb877
MaizeHH <92512497+MaizeHH@users.noreply.github.com>
2023-09-21 15:50:53 +0200

    enter new password page
    still needs password validation and functionality

--- eml/components/login/ResetPassword.js
+++ eml/components/login/ResetPassword.js
@@ -2,48 +2,72 @@ import { Text, View, Animated, Modal, Pressable } from "react-native";
 import { React, useEffect, useState } from "react";
 import FormTextField from "./FormTextField";
 import FormButton from "./FormButton";
+import { Entypo } from '@expo/vector-icons';
 
 export default function ResetPassword(props) {
 
 	const [emailSent, setEmailSent] = useState(false);
+	const [codeEntered, setCodeEntered] = useState(false);
 
 	const closeModal = () => {
 		props.onModalClose();
 	}
 
+
+
 	return (
-		<Modal visible={props.modalVisible} animationType='slide' className='border-8 border-black'>
-			<View className='flex-row py-[40px]'>
+		<Modal visible={props.modalVisible} animationType='slide' className='border-8 border-black bg-modalBackground'>
+			<View className='flex-row py-[40px] justify-center'>
 				<Text className='text-center text-[24px] mx-10'>Redefinção de senha</Text>
 				<Pressable onPress={closeModal}>
-					<Text>Close modal</Text>
+					<Entypo name='chevron-down' size={24} />
 				</Pressable>
 			</View>
 			<View className='my-[80px]'>
-				<FormTextField
-					placeholder='user@email.com'
-					label='Email'
-					required={true}
-					onChangeText={''}
-				/>
-				<View className='mt-[40px]'>
-					{emailSent ?
-						<View>
-							<Text className='text-center mx-10 mb-[40px]'>Enviamos um código par ao seu email de redefinição de senha, por favor, insira o mesmo abaixo</Text>
-							<FormTextField
-								placeholder='X X X X'
-								onChangeText={''}
-							/>
-							<View className='mt-[40px] mb-[24px]'>
-								<FormButton label='Continuar' />
-							</View>
-							<View className='mx-10 flex-row'>
-								<Text className='text-center' >O código não chegou?</Text>
-								<Text className='underline ml-1' >Reenviar cógio</Text>
-							</View>
+				{!codeEntered ?
+					<View>
+						<FormTextField
+							placeholder='user@email.com'
+							label='Email'
+							required={true}
+							onChangeText={''}
+						/>
+						<View className='mt-[40px]'>
+							{emailSent ?
+								<View>
+									<Text className='text-center mx-10 mb-[40px]'>Enviamos um código par ao seu email de redefinição de senha, por favor, insira o mesmo abaixo</Text>
+									<FormTextField
+										placeholder='X X X X'
+										onChangeText={''}
+									/>
+									<View className='mt-[40px] mb-[24px]'>
+										<FormButton label='Continuar' onPress={() => setCodeEntered(true)} />
+									</View>
+									<View className='mx-10 flex-row justify-center'>
+										<Text >O código não chegou?</Text>
+										<Text className='underline ml-1' >Reenviar cógio</Text>
+									</View>
+								</View>
+								: <FormButton label='Enviar código' onPress={() => setEmailSent(true)} />}
 						</View>
-						: <FormButton label='Enviar código' onPress={() => setEmailSent(true)} />}
-				</View>
+					</View>
+					: <View>
+						<FormTextField
+							placeholder='Senha'
+							onChangeText={''}
+							label='Nova senha'
+							required={true}
+						/>
+						<View className='mt-[24px] mb-[40px]'>
+							<FormTextField
+								placeholder='Senha'
+								onChangeText={''}
+								label='Confirmar nova senha'
+								required={true}
+							/>
+						</View>
+						<FormButton label='Entrar' />
+					</View>}
 			</View>
 		</Modal>
 	)
