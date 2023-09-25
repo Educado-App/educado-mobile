8e2874ea539a719d17e7b7c75e28a5e048af2343
Mikkel Christensen <mmc.99@hotmail.com>
2023-09-25 16:39:02 +0200

    1
    1

--- eml/components/navBar/NavBar.jsx
+++ eml/components/navBar/NavBar.jsx
@@ -15,7 +15,39 @@ export default function NavBar() {
       screenOptions={{
         tabBarActiveTintColor: 'black',
         tabBarActiveBackgroundColor: '#d9d9d9',
-        tabBarStyle: { backgroundColor: 'hsl(0, 0%, 92%)' }, // OnePlus menubar color
+        tabBarLabelStyle: {
+          fontSize: 11.5,
+          fontFamily: 'VarelaRound_400Regular',
+        },
+        
+        tabBarStyle: {
+          backgroundColor: 'white',
+          paddingVertical: '2%',
+          paddingHorizontal: '4%',
+          height: '10%',
+          
+          // THIS IS SHADOW STUFF - HAVE TO BE PLATFORM SPECIFIC
+          ...Platform.select({
+            ios: {
+              shadowColor: 'rgba(0, 0, 0, 0.2)',
+              shadowOffset: {
+                width: 0,
+                height: 1,
+              },
+              shadowOpacity: 0.8,
+              shadowRadius: 8,
+            },
+            android: {
+              elevation: 4, // Add elevation for the shadow (Android-specific)
+            },
+          }),
+        },
+        tabBarItemStyle: {
+          borderRadius: 15,
+          marginHorizontal: '2%', // Adjust the margin for spacing
+          paddingBottom: '2%', // Vertical padding for the icon
+          paddingTop: '1%', // Vertical padding for the icon
+        },
       }}
     >
       <Tab.Screen
@@ -26,7 +58,7 @@ export default function NavBar() {
           headerShown: false,
           tabBarIcon: ({ color }) => ( // Pass the color as a parameter to the icon component
             <Icon
-              size={20}
+              size={17}
               name="home-outline"
               type="material-community"
               color={color} // Use the color parameter here
@@ -44,7 +76,7 @@ export default function NavBar() {
           headerShown: false,
           tabBarIcon: ({ color }) => ( // Pass the color as a parameter to the icon component
             <Icon
-              size={20}
+              size={17}
               name="compass-outline"
               type="material-community"
               color={color} // Use the color parameter here
@@ -62,7 +94,7 @@ export default function NavBar() {
           headerShown: false,
           tabBarIcon: ({ color }) => ( // Pass the color as a parameter to the icon component
             <Icon
-              size={20}
+              size={17}
               name="robot-outline"
               type="material-community"
               color={color} // Use the color parameter here
@@ -72,15 +104,16 @@ export default function NavBar() {
           tabBarInactiveTintColor: 'grey', // Set the inactive text color to grey
         }}
       />
-      <Tab.Screen
+      {/* <Tab.Screen
         name="TestScreen"
         component={TestScreen}
+
         options={{
           tabBarActiveBackgroundColor: '#5fcce9',
           headerShown: false,
           tabBarIcon: ({ color }) => ( // Pass the color as a parameter to the icon component
             <Icon
-              size={20}
+              size={17}
               name="bug-outline"
               type="material-community"
               color={color} // Use the color parameter here
@@ -89,7 +122,7 @@ export default function NavBar() {
           tabBarActiveTintColor: 'white', // Set the active text color to white
           tabBarInactiveTintColor: 'grey', // Set the inactive text color to grey
         }}
-      />
+      /> */}
       <Tab.Screen
         name="Profile"
         component={ProfileComponent}
@@ -98,7 +131,7 @@ export default function NavBar() {
           headerShown: false,
           tabBarIcon: ({ color }) => ( // Pass the color as a parameter to the icon component
             <Icon
-              size={20}
+              size={17}
               name="account-outline"
               type="material-community"
               color={color} // Use the color parameter here
