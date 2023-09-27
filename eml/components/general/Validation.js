5c637af3fcb68ba2ee2d2ccafe8e68cd09e25578
Jatewo <jacobterpewoods@gmail.com>
2023-09-27 14:23:59 +0200

    Refactor of register to use RemoveEmojis + react hook error fix

--- eml/components/general/Validation.js
+++ eml/components/general/Validation.js
@@ -1,7 +1,7 @@
 /**
    * Function for validating the password input. It checks if the password contains any emojis and if so it does not change the password state variable.
-   * @param {Function} setPasswordFunction 
-   * @param {string} passwordInput current input in password field
+   * @param {string} passwordInput input in password field
+   * @param {string} currentPasword current password state variable
    * @returns either the password state variable or the confirm password state variable depending on the confirm parameter
    */
 const RemoveEmojis = (passwordInput, currentPasword) => {
