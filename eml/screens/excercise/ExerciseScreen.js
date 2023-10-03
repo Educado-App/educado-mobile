8a01a83d0712a6ba3e51763546b4131cc79acb48
Andazu <andersmazen@gmail.com>
2023-10-03 12:23:41 +0200

    Exercise screen updates
    Added multiple options, radio buttons, and background

--- eml/screens/excercise/ExerciseScreen.js
+++ eml/screens/excercise/ExerciseScreen.js
@@ -1,23 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useRef, useState } from "react";
-import { Alert, StyleSheet, View, Text } from "react-native";
+import { ScrollView, StyleSheet, View, Text } from "react-native";
 import LeaveButton from "../../components/exercise/LeaveButton";
 import ExerciseButtons from "../../components/exercise/ExerciseButtons";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Video } from "expo-av";
 import * as StorageService from "../../services/StorageService";
 import CustomProgressBar from "../../components/exercise/Progressbar";
-import dummyExerciseData from './dummyExerciseData.json';
-
-
+import dummyExerciseData from "./dummyExerciseData.json";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { RadioButton } from "react-native-paper";
 
 export default function ExerciseScreen() {
   const navigation = useNavigation();
   const route = useRoute();
-  
+
   const [hasData, setHasData] = useState(false);
   const [signal, setSignal] = useState([]);
   const [exerciseData, setExerciseData] = useState({});
+  const [selectedAnswer, setSelectedAnswer] = useState(null); // State to store the selected answer
+
+  const handleAnswerSelect = (answerId) => {
+    setSelectedAnswer(answerId); // Update the selected answer when a radio button is pressed
+  };
 
   /*async function getExercise() {
     const exercise = await StorageService.getNextExercise(sectionId);
@@ -41,15 +45,15 @@ export default function ExerciseScreen() {
   async function getExercise() {
     // Instead of calling StorageService.getNextExercise, use the dummy data
     const exercise = dummyExerciseData;
-  
+
     if (exercise !== null) {
       console.log(exercise);
-  
-     /* const courseId = exercise.courseId || "defaultCourseId";
+
+      /* const courseId = exercise.courseId || "defaultCourseId";
       const sectionId = exercise.sectionId || "defaultSectionId";*/
-  
+
       if (exercise === true) {
-       /* navigation.navigate("SectionComplete", {
+        /* navigation.navigate("SectionComplete", {
           courseId: courseId,
           sectionId: sectionId,
         });*/
@@ -60,8 +64,6 @@ export default function ExerciseScreen() {
       navigation.navigate("ErrorScreen");
     }
   }
-  
-  
 
   /*async function isSectionComplete(courseId, sectionId) {
     const course = await StorageService.getCourseById(courseId);
@@ -81,103 +83,73 @@ export default function ExerciseScreen() {
     });
   }, [route.params]);
 
-  const video = useRef(0);
-
-  useEffect(() => {
-    if (video !== undefined) {
-      if (signal === 0) {
-        try {
-          video.current.pauseAsync();
-        } catch (e) {
-          console.log("Something went wrong :" + e);
-        }
-      }
-      setSignal(1);
-    }
-  }, [signal]);
-
   return (
-    <View style={styles.container} className="bg-babyBlue">
-      <View style={{ flex: 1 }}>
-        <View className="pt-8 flex-row w-screen">
-          <View className="">
-            <LeaveButton
+    <View className="bg-secondary flex-1">
+      <SafeAreaView>
+        <View>
+          <View>
+            <View>
+              <LeaveButton
               //navigationPlace={"Course"}
               //courseId={courseId}
-            ></LeaveButton>
+              ></LeaveButton>
+            </View>
           </View>
-          <View
-            style={{
-              left: "80%",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          ></View>
+          <View className="items-center">
+            <CustomProgressBar progress={0.5 / 1}></CustomProgressBar>
+          </View>
         </View>
-        <View className="items-center">
-          <CustomProgressBar progress={0.5 / 1}></CustomProgressBar>
-        </View>
-      </View>
-      <View style={{ flex: 2, width: "100%" }}>
-        {exerciseData === undefined ? (
+
+        {dummyExerciseData === undefined ? (
           // No Data
           <Text> Sem dados</Text>
         ) : (
-          <Video
-            source={{ uri: exerciseData.content }}
-            rate={1.0}
-            volume={1.0}
-            isMuted={false}
-            resizeMode="cover"
-            shouldPlay
-            useNativeControls
-            ref={video}
-            style={styles.backgroundVideo}
-          />
+          <View className="items-center">
+            <Text className="text-center font-montserrat-bold">
+              {dummyExerciseData.question}
+            </Text>
+
+            <ScrollView>
+              {/* Map through the answers and render each one */}
+              {dummyExerciseData.answers.map((answer) => (
+                <View
+                  key={answer.id}
+                  style={{ flexDirection: "row", alignItems: "center" }}
+                >
+                  <RadioButton
+                    value={answer.id}
+                    status={
+                      selectedAnswer === answer.id ? "checked" : "unchecked"
+                    }
+                    onPress={() => handleAnswerSelect(answer.id)}
+                  />
+                  <Text> {answer.text}</Text>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
         )}
-      </View>
-      <View style={{ flex: 3 }}>
-        {exerciseData === undefined ? (
-          //No data
-          <Text> Sem dados</Text>
-        ) : (
-          <ExerciseButtons
-            answers={exerciseData.answers}
-            exerciseId={exerciseData.id}
-            courseId={exerciseData.courseId}
-            sectionId={exerciseData.sectionId}
-            setSignal={setSignal}
-            hasData={hasData}
-          ></ExerciseButtons>
-        )}
-      </View>
-      <StatusBar style="auto" />
+
+        {/* Old exercise buttons
+        <View style={{ flex: 3 }}>
+          {exerciseData === undefined ? (
+            //No data
+            <Text> Sem dados</Text>
+          ) : (
+            <ExerciseButtons
+              answers={exerciseData.answers}
+              exerciseId={exerciseData.id}
+              courseId={exerciseData.courseId}
+              sectionId={exerciseData.sectionId}
+              setSignal={setSignal}
+              hasData={hasData}
+            ></ExerciseButtons>
+          )}
+        </View>
+        */}
+
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  textStyle: {
-    zIndex: 100,
-    fontSize: 100,
-  },
-  buttonShadow: {
-    shadowOpacity: 0.4,
-    shadowRadius: 10,
-    shadowOffset: { width: 0, height: 0 },
-  },
-  backgroundVideo: {
-    height: "100%",
-  },
-});
