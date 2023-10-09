3a38463ff42d4c1b3875849f5050c9b8289f6951
PrometheusBork <tobiasmerithz@gmail.com>
2023-10-09 18:18:29 +0200

    Adjusted size for progress bar

--- eml/components/courses/courseHeader/CourseProgress.jsx
+++ eml/components/courses/courseHeader/CourseProgress.jsx
@@ -19,9 +19,9 @@ export default function CourseProgress({ fracTop, fracBot }) {
   const progressWidth = (fracTop / fracBot)*100;
 
   return (
-    <View className="flex-col h-[45%] bg-[#ccc] rounded-[10px] my-[10px] w-[100%] max-w-[71%] min-w-[71%]">
+    <View className="flex-col h-[45%] bg-[#ccc] rounded-[10px] my-[10px] w-[100%] max-w-[65%] min-w-[65%]">
       <View className={'rounded-tl-[10px] rounded-bl-[10px] rounded-br-[8px] rounded-tr-[8px] h-[100%] bg-[#5ECCE9] opacity-[0.5] absolute min-w-[0%] w-[' + progressWidth.toString() + '%]'}/>
-      <Text className="text-[13px] font-bold text-black absolute bottom-[0%] right-[-28%]">
+      <Text className="text-[13px] font-bold text-black absolute bottom-[0%] right-[-30%]">
         {fracTop} / {fracBot}
       </Text>
     </View> 
