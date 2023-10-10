8270d3e6d629dd56ca8ef531e74c124c3b11ca7b
MaizeHH <92512497+MaizeHH@users.noreply.github.com>
2023-10-10 12:08:40 +0200

    login page scrollable with keyboard
    - added ScrollView and KeyboardAwareScrollView
    - able to scroll to bottom most componenet when keyboard is toggled
    
    Co-Authored-By: Morten Munk <92527083+MortenMunk@users.noreply.github.com>

--- eml/screens/login/Login.js
+++ eml/screens/login/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Keyboard } from "react-native";
+import { View, Keyboard, ScrollView } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import LoginForm from "../../components/login/LoginForm";
@@ -7,6 +7,7 @@ import LogoBackButton from "../../components/login/LogoBackButton";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TouchableWithoutFeedback } from "react-native";
 import Text from "../../components/general/Text";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const LOGIN_TOKEN = "@loginToken";
 
@@ -38,37 +39,44 @@ export default function Login(props) {
   useEffect(() => {
     checkLoginToken();
   }, []);
-  
+
   return (
     <SafeAreaView className="justify-start bg-secondary flex-1">
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View>
-          <View className="mt-10">
-            <LogoBackButton navigationPlace="Login" />
-          </View>
-          <View className="mx-6">
-            {/* Login form */}
-            <View className="my-8">
-              <LoginForm />
+      <ScrollView className="flex-1">
+        <KeyboardAwareScrollView
+          className="flex-1"
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          scrollEnabled={false}>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View>
+              <View className="mt-10">
+                <LogoBackButton navigationPlace="Login" />
+              </View>
+              <View className="mx-6">
+                {/* Login form */}
+                <View className="my-8">
+                  <LoginForm />
+                </View>
+                {/* Register button */}
+                <View className="flex-row justify-center">
+                  <Text className="text-base text-gray mr-1">
+                    {/* Dont have an account yet? */}
+                    Ainda não tem conta?
+                  </Text>
+                  <Text
+                    testId="registerNav"
+                    className={"text-base text-black underline"}
+                    onPress={() => navigation.navigate("Register")}
+                  >
+                    {/* Sign up now */}
+                    Cadastre-se agora
+                  </Text>
+                </View>
+              </View>
             </View>
-            {/* Register button */}
-            <View className="flex-row justify-center">
-              <Text className="text-base text-gray mr-1">
-                {/* Dont have an account yet? */}
-                Ainda não tem conta? 
-              </Text>
-              <Text
-                testId="registerNav"
-                className={"text-base text-black underline"}
-                onPress={() => navigation.navigate("Register")}
-              >
-                {/* Sign up now */}
-                Cadastre-se agora
-              </Text>
-            </View>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </SafeAreaView>
+          </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>
+      </ScrollView>
+    </SafeAreaView >
   );
 }
