4638d79534288206caf0368a69d2ec419616c62a
MortenMunk <mortenmunk97@gmail.com>
2023-10-13 15:53:02 +0200

    Added gradient to reset password modal
    installed expo-linear-gradient

--- eml/components/general/EducadoModal.js
+++ eml/components/general/EducadoModal.js
@@ -3,6 +3,7 @@ import { View, Modal, Pressable, TouchableWithoutFeedback, Keyboard } from 'reac
 import { Entypo } from '@expo/vector-icons';
 import EducadoLogo from '../images/EducadoLogo';
 import Text from './Text';
+import { BgLinearGradient } from '../../constants/BgLinearGradient';
 
 /**
  * 
@@ -18,26 +19,28 @@ export default function EducadoModal(props) {
     <Modal
       visible={props.modalVisible}
       animationType="slide"
-      className="border-8 border-black bg-modalBackground"
+      className="border-8 border-black"
     >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View>
-          <View className="flex justify-center pt-[40px]">
-            <View className="flex flex-row justify-end px-10">
-              <Pressable onPress={props.closeModal}>
-                <Entypo name="chevron-down" size={24} />
-              </Pressable>
+      <BgLinearGradient>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <View className="flex justify-center pt-[40px]">
+              <View className="flex flex-row justify-end px-10">
+                <Pressable onPress={props.closeModal}>
+                  <Entypo name="chevron-down" size={24} />
+                </Pressable>
+              </View>
+              <View className="flex flex-row justify-center my-10">
+                <EducadoLogo fill='fill-black' />
+              </View>
+              <View className="flex flex-row justify-start px-10">
+                <Text className="text-center text-[24px]">{props.title ? props.title : ""}</Text>
+              </View>
             </View>
-            <View className="flex flex-row justify-center my-10">
-              <EducadoLogo fill='fill-black' />
-            </View>
-            <View className="flex flex-row justify-start px-10">
-              <Text className="text-center text-[24px]">{props.title ? props.title : ""}</Text>
-            </View>
+            {props.children}
           </View>
-          {props.children}
-        </View>
-      </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      </BgLinearGradient>
     </Modal >
   )
 }
