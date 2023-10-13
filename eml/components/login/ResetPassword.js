f0e1c417dd904bc4c06a09f6a7acf9b3fe6f37f4
MaizeHH <92512497+MaizeHH@users.noreply.github.com>
2023-10-13 13:59:37 +0200

    Email can be sent from frontend
    email entered in field triggers the "reset-password-request" request

--- eml/components/login/ResetPassword.js
+++ eml/components/login/ResetPassword.js
@@ -5,6 +5,7 @@ import FormButton from "./FormButton";
 import EducadoModal from "../general/EducadoModal";
 import EnterNewPasswordScreen from "./EnterNewPasswordScreen";
 import Text from '../general/Text';
+import { sendResetPasswordEmail } from "../../api/userApi";
 
 /**
  * Component to create modal (popup) that prompts user for
@@ -14,59 +15,82 @@ import Text from '../general/Text';
  * - onModalClose: Function to do when modal closes
  */
 export default function ResetPassword(props) {
+  const [email, setEmail] = useState("");
   const [emailSent, setEmailSent] = useState(false);
   const [codeEntered, setCodeEntered] = useState(false);
 
+  async function sendEmail(email) {
+    const obj = {
+      email,
+    };
+
+    console.log("Sending email: " + email);
+
+    try {
+      await sendResetPasswordEmail(obj)
+        .then( async () => {
+          console.log("Email sent");
+          console.log(email);
+          setEmailSent(true);
+        }).catch((error) => {
+          console.log(error + "YUH"); //HERE
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <EducadoModal modalVisible={props.modalVisible} closeModal={props.onModalClose} id="EducadoModal" title="Redefinção de senha">
-        <View className="my-[80px] px-10">
-          {!codeEntered ? (
-            <View>
-              <FormTextField
-                bordered={true}
-                placeholder="user@email.com"
-                label="Email"
-                required={true}
-                onChangeText={""}
-              />
-              <View className="mt-[40px]">
-                {emailSent ? (
-                  <View>
-                    <Text className="text-left mb-[10px]">
-                      {/* We have sent a code to your mail to reset password,
+      <View className="my-[80px] px-10">
+        {!codeEntered ? (
+          <View>
+            <FormTextField
+              bordered={true}
+              placeholder="user@email.com"
+              label="Email"
+              required={true}
+              onChangeText={(email) => setEmail(email)}
+              keyboardType="email-address"
+            />
+            <View className="mt-[40px]">
+              {emailSent ? (
+                <View>
+                  <Text className="text-left mb-[10px]">
+                    {/* We have sent a code to your mail to reset password,
                     please enter the same code below */}
-                      Enviamos um código par ao seu email de redefinição de senha,
-                      por favor, insira o mesmo abaixo
-                    </Text>
-                    <FormTextField placeholder="X X X X" onChangeText={""} />
-                    <View className="mt-[40px] mb-[24px]">
-                      <FormButton
-                        // Continue 
-                        label="Continuar"
-                        onPress={() => setCodeEntered(true)}
-                      />
-                    </View>
-                    <View className="mx-10 flex-row justify-center">
-                      {/* Didn't the code arrive?*/}
-                      <Text>O código não chegou?</Text>
-                      {/* Resend code*/}
-                      <Text className="underline ml-1">Reenviar cógio</Text>
-                    </View>
+                    Enviamos um código par ao seu email de redefinição de senha,
+                    por favor, insira o mesmo abaixo
+                  </Text>
+                  <FormTextField placeholder="X X X X" onChangeText={""} />
+                  <View className="mt-[40px] mb-[24px]">
+                    <FormButton
+                      // Continue 
+                      label="Continuar"
+                      onPress={() => setCodeEntered(true)}
+                    />
                   </View>
-                ) : (
-                  <FormButton
-                    // Send code
-                    label="Enviar código"
-                    onPress={() => setEmailSent(true)}
-                  />
-                )}
-              </View>
+                  <View className="mx-10 flex-row justify-center">
+                    {/* Didn't the code arrive?*/}
+                    <Text>O código não chegou?</Text>
+                    {/* Resend code*/}
+                    <Text className="underline ml-1">Reenviar cógio</Text>
+                  </View>
+                </View>
+              ) : (
+                <FormButton
+                  // Send code
+                  label="Enviar código"
+                  onPress={() => sendEmail(email)}
+                />
+              )}
             </View>
-          ) : (
-            <EnterNewPasswordScreen />
-          )}
-        </View>
-   
+          </View>
+        ) : (
+          <EnterNewPasswordScreen />
+        )}
+      </View>
+
     </EducadoModal>
   );
 }
