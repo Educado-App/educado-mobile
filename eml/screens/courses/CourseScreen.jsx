05b8694790b1b147d4f95aa18452540e3d626977
sofiiagran <sofiiagran@gmail.com>
2023-10-19 14:57:45 +0200

    coursescreen update, no longer infinite loop

--- eml/screens/courses/CourseScreen.jsx
+++ eml/screens/courses/CourseScreen.jsx
@@ -1,9 +1,9 @@
-import { useNavigation } from '@react-navigation/native'
-import React, { useEffect, useState } from 'react'
-import { View, Pressable, Image, ScrollView } from 'react-native'
-import Text from '../../components/general/Text'
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { View, Pressable, Image, ScrollView, RefreshControl } from 'react-native';
+import Text from '../../components/general/Text';
 import * as StorageService from "../../services/StorageService";
-import CourseCard from '../../components/courses/courseCard/CourseCard'
+import CourseCard from '../../components/courses/courseCard/CourseCard';
 import BaseScreen from '../../components/general/BaseScreen';
 import IconHeader from '../../components/general/IconHeader';
 
@@ -12,39 +12,67 @@ import IconHeader from '../../components/general/IconHeader';
  * @component
  * @returns {JSX.Element} The course screen component.
  */
+
 export default function CourseScreen() {
 
     /**
-     * React hook that declares a state variable for courses and a function to update it.
-     * @typedef {[Object[], function]} CourseState
-     * @returns {CourseState} The state variable and its updater function.
-     */
+    * React hook that declares a state variable for courses and a function to update it.
+    * @typedef {[Object[], function]} CourseState
+    * @returns {CourseState} The state variable and its updater function.
+    */
     const [courses, setCourses] = useState([]);
 
     const [courseLoaded, setCourseLoaded] = useState(false);
 
+    const [refreshing, setRefreshing] = useState(false);
+
     const navigation = useNavigation()
 
-    /**
-         * Asynchronous function that loads the courses from storage and updates the state.
-         * @returns {void}
-         */
-    async function loadCourses() {
-        const courseData = await StorageService.getSubCourseList();
-        if (courseData.length !== 0 && Array.isArray(courseData)) {
-            setCourses(courseData);
-            setCourseLoaded(true);
-        } else {
-            setCourses([]);
-            setCourseLoaded(false);
+    // Compare two course arrays to check for equality
+    function areCoursesEqual(courses1, courses2) {
+        if (courses1.length !== courses2.length) {
+            return false;
         }
 
-    
+        for (let i = 0; i < courses1.length; i++) {
+            if (courses1[i].id !== courses2[i].id) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    useEffect(() => {
+    /**
+    * Asynchronous function that loads the courses from storage and updates the state.
+    * @returns {void}
+    */
+    async function loadCourses() {
+        const courseData = await StorageService.getSubCourseList();
+        if (!areCoursesEqual(courses, courseData)) {
+            if (courseData.length !== 0 && Array.isArray(courseData)) {
+                setCourses(courseData);
+                setCourseLoaded(true);
+            } else {
+                setCourses([]);
+                setCourseLoaded(false);
+            }
+        }
+    }
+
+    // When refreshing the loadCourses function is called
+    const onRefresh = () => {
+        setRefreshing(true);
         loadCourses();
-    }, [courses]);
+        setRefreshing(false);
+    };
+
+    useEffect(() => {
+        // this makes sure loadcourses is called when the screen is focused
+        const update = navigation.addListener('focus', () => {
+            loadCourses();
+        });
+        return update;
+    }, [navigation]);
 
     return (
         <BaseScreen>
@@ -55,7 +83,7 @@ export default function CourseScreen() {
             {courseLoaded ?
                 <View height="100%">
                     <IconHeader title={"Bem Vindo!"} />
-                    <ScrollView showsVerticalScrollIndicator={false}>
+                    <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
                         {courses.map((course, index) => (
                             <CourseCard key={index} course={course}></CourseCard>
                         )
