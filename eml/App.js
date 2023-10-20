a8e8a94559cadec9387488346bd6b4aad3e7681f
Andazu <andersmazen@gmail.com>
2023-10-20 14:19:44 +0200

    Revert "Merge branch 'dev-copy' into gamified_exercies-frontend"
    This reverts commit 26bf3462cf8d4038bfe6e4babd0f4204a54fdc0d, reversing
    changes made to 2046b1e12d08c5f812c386c7083a4bfb6ca85421.

--- eml/App.js
+++ eml/App.js
@@ -18,7 +18,6 @@ import { TailwindProvider } from 'tailwindcss-react-native';
 import ErrorScreen from './screens/errors/ErrorScreen';
 import SectionCompleteScreen from './screens/excercise/SectionCompleteScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import SectionScreen from './screens/section/SectionScreen';
 import { isFontsLoaded } from './constants/Fonts';
 import Loading from "./components/loading/Loading";
 import WelcomeScreen from "./screens/welcome/Welcome";
@@ -26,7 +25,6 @@ import ProfileSettingsScreen from "./screens/profile/ProfileSettings";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
 /**
  * Check if user is logged in, if not redirect to login screen
  */
@@ -262,14 +260,8 @@ export default function App() {
                 options={{ headerShown: false }}
               />
               <Stack.Screen
-                name={'HomeStack'}
-                component={HomeStack} // Use the NavBar component here
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Section"
-                component={SectionScreen}
-                initialParams={{ course_id: '' }}
+                name={"HomeStack"}
+                component={HomeStack}
                 options={{ headerShown: false }}
               />
               <Stack.Screen
