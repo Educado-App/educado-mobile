d245aaa648f995973f3aaf3b923f1906d7250420
Andazu <andersmazen@gmail.com>
2023-10-23 16:04:19 +0200

    Updated api and userApi

--- eml/api/api.js
+++ eml/api/api.js
@@ -1,50 +1,12 @@
 import axios from "axios";
 
 const testUrl = "http://localhost:8888";
-const testExpo = "http://172.30.210.200:8888"; //Change to local expo ip
+const testExpo = "http://172.30.245.130:8888"; //Change to local expo ip
 const digitalOcean = "http://207.154.213.68:8888";
 
-const url = testExpo;
+const url = testUrl;
 
-// TODO: Find a solution to refresh auth-token
-const authToken = "";
-const authBody = {
-  email: "demo@gmail.com",
-  password: "Demo1234",
-};
-const config = {
-  headers: {
-    Authorization: "Bearer " + authToken,
-  },
-};
-
-export const getAuthToken = async () => {
-  const res = await axios.post(url + "/auth/jwt", authBody);
-  return res.data;
-};
-
-export const getTestCourse = async () => {
-  const res = await axios.get(
-    url + "/api/public/courses/635fb5b9b2fb6c4f49084682"
-  );
-  return res.data;
-};
-
-export const getCoursesWithAuth = async () => {
-  const res = await axios.get(url + "/api/courses", config);
-  return res.data;
-};
-
-export const getCourseWithAuth = async (courseId) => {
-  const res = await axios.get(url + "/api/courses/" + courseId, config);
-  return res.data;
-};
-
-export const getCourses = async () => {
-  // TODO: add bearer token to request header and omit /public
-  const res = await axios.get(url + "/api/public/courses");
-  return res.data;
-};
+/*** COURS, SECTIONS AND EXERCISES ***/
 
 export const getCourseByid = async (courseId) => {
   const res = await axios.get(url + "/api/courses/" + courseId);
@@ -61,40 +23,159 @@ export const getExerciseByid = async (exerciseId) => {
   return res.data;
 };
 
-// TODO: Endpoint for getcoursebyid && change getCourses to getCourseList
+// Get specific course
 
-export const getPresignedUrl = async (component_id) => {
-  const obj = {
-    component_id,
-  };
-  const res = await axios.post(url + "/api/get-presigned-url", obj);
-  return res.data;
+export const getCourse = async (courseId) => {
+  try {
+    const res = await axios.get(url + "/api/courses/" + courseId);
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
 };
 
-export const getCoverPhoto = async (course_id) => {
-  const obj = {
-    course_id,
-  };
-  // Send request to S3 server
-  const res = await axios.post(url + "/api/eml/get-presigned-url", obj);
-  return res.data;
+// Get all courses
+export const getCourses = async () => {
+  try {
+    const res = await axios.get(url + "/api/courses");
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
 };
 
-export const getAllSections = async (sections) => {
-  const obj = {
-    sections,
-  };
-  // Send request to S3 server
-  const res = await axios.post(url + "/api/eml/courses/getallsections", obj);
-
-  return res.data;
+// Get all sections for a specific course
+export const getAllSections = async (courseId) => {
+  try {
+    const res = await axios.get(url + "/api/courses/" + courseId + "/sections");
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
 };
 
-export const getAllComponents = async (components) => {
-  const obj = {
-    components,
-  };
-  // Send request to S3 server
-  const res = await axios.post(url + "/api/component/getallcomponents", obj);
-  return res.data;
+// Get specific section
+export const getSection = async (courseId, sectionId) => {
+  try {
+    const res = await axios.get(
+      url + "/api/courses/" + courseId + "/sections/" + sectionId
+    );
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
+};
+
+// Get all exercises in a specific section:
+export const getExercisesInSection = async (courseId, sectionId) => {
+  try {
+    const res = await axios.get(
+      url + "/api/courses/" + courseId + "/sections/" + sectionId + "/exercises"
+    );
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
+};
+
+/*** SUBSCRIPTION ***/
+
+// Get user subsribtions
+export const getSubscriptions = async (userId) => {
+  try {
+    // maybe not best practise to pass user ID as request query
+    // but this is the only format where it works
+    // passing user ID as request body for get request gives error
+    const res = await axios.get(
+      url + "/api/users/" + userId + "/subscriptions"
+    );
+
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
+};
+
+// Subscribe to course
+export const subscribeToCourse = async (userId, courseId) => {
+  try {
+    const res = await axios.post(
+      url + "/api/courses/" + courseId + "/subscribe",
+      {
+        user_id: userId,
+      }
+    );
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
+};
+
+// Unubscribe to course
+export const unSubscribeToCourse = async (userId, courseId) => {
+  try {
+    const res = await axios.post(
+      url + "/api/courses/" + courseId + "/unsubscribe",
+      {
+        user_id: userId,
+      }
+    );
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
+};
+
+export const ifSubscribed = async (userId, courseId) => {
+  try {
+    // maybe not best practise to pass user ID as request query
+    // but this is the only format where it works
+    // passing user ID as request body for get request gives error
+    const res = await axios.get(
+      url +
+        "/api/users/subscriptions?user_id=" +
+        userId +
+        "&" +
+        "course_id=" +
+        courseId
+    );
+
+    return res.data;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
+  }
 };
