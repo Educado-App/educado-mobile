b6d0b1c37069ca5bc40232641a42d4e35ef00e43
Andazu <andersmazen@gmail.com>
2023-10-23 15:54:12 +0200

    Revert "Updated api"
    This reverts commit 722e780f7a6eeacad6f0bc34a70f440bd2276301.

--- eml/api/api.js
+++ eml/api/api.js
@@ -4,83 +4,46 @@ const testUrl = "http://localhost:8888";
 const testExpo = "http://172.30.210.200:8888"; //Change to local expo ip
 const digitalOcean = "http://207.154.213.68:8888";
 
-const url = testUrl;
+const url = testExpo;
 
-/*** COURS, SECTIONS AND EXERCISES ***/
-
-// Get specific course
-
-export const getCourse = async (courseId) => {
-  try {
-    const res = await axios.get(url + "/api/courses/" + courseId);
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+// TODO: Find a solution to refresh auth-token
+const authToken = "";
+const authBody = {
+  email: "demo@gmail.com",
+  password: "Demo1234",
+};
+const config = {
+  headers: {
+    Authorization: "Bearer " + authToken,
+  },
 };
 
-// Get all courses
+export const getAuthToken = async () => {
+  const res = await axios.post(url + "/auth/jwt", authBody);
+  return res.data;
+};
+
+export const getTestCourse = async () => {
+  const res = await axios.get(
+    url + "/api/public/courses/635fb5b9b2fb6c4f49084682"
+  );
+  return res.data;
+};
+
+export const getCoursesWithAuth = async () => {
+  const res = await axios.get(url + "/api/courses", config);
+  return res.data;
+};
+
+export const getCourseWithAuth = async (courseId) => {
+  const res = await axios.get(url + "/api/courses/" + courseId, config);
+  return res.data;
+};
+
 export const getCourses = async () => {
-  try {
-    const res = await axios.get(url + "/api/courses");
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-// Get all sections for a specific course
-export const getAllSections = async (courseId) => {
-  try {
-    const res = await axios.get(url + "/api/courses/" + courseId + "/sections");
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-// Get specific section
-export const getSection = async (courseId, sectionId) => {
-  try {
-    const res = await axios.get(
-      url + "/api/courses/" + courseId + "/sections/" + sectionId
-    );
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-// Get all exercises in a specific section:
-export const getExercisesInSection = async (courseId, sectionId) => {
-  try {
-    const res = await axios.get(
-      url + "/api/courses/" + courseId + "/sections/" + sectionId + "/exercises"
-    );
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+  // TODO: add bearer token to request header and omit /public
+  const res = await axios.get(url + "/api/public/courses");
+  return res.data;
 };
 
 export const getCourseByid = async (courseId) => {
@@ -98,84 +61,40 @@ export const getExerciseByid = async (exerciseId) => {
   return res.data;
 };
 
-/*** SUBSCRIPTION ***/
+// TODO: Endpoint for getcoursebyid && change getCourses to getCourseList
 
-// Get user subsribtions
-export const getSubscriptions = async (userId) => {
-  try {
-    // maybe not best practise to pass user ID as request query
-    // but this is the only format where it works
-    // passing user ID as request body for get request gives error
-    const res = await axios.get(
-      url + "/api/users/" + userId + "/subscriptions"
-    );
-
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+export const getPresignedUrl = async (component_id) => {
+  const obj = {
+    component_id,
+  };
+  const res = await axios.post(url + "/api/get-presigned-url", obj);
+  return res.data;
 };
 
-// Subscribe to course
-export const subscribeToCourse = async (userId, courseId) => {
-  try {
-    const res = await axios.post(
-      url + "/api/courses/" + courseId + "/subscribe",
-      {
-        user_id: userId,
-      }
-    );
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+export const getCoverPhoto = async (course_id) => {
+  const obj = {
+    course_id,
+  };
+  // Send request to S3 server
+  const res = await axios.post(url + "/api/eml/get-presigned-url", obj);
+  return res.data;
 };
 
-// Unubscribe to course
-export const unSubscribeToCourse = async (userId, courseId) => {
-  try {
-    const res = await axios.post(
-      url + "/api/courses/" + courseId + "/unsubscribe",
-      {
-        user_id: userId,
-      }
-    );
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+export const getAllSections = async (sections) => {
+  const obj = {
+    sections,
+  };
+  // Send request to S3 server
+  const res = await axios.post(url + "/api/eml/courses/getallsections", obj);
+
+  return res.data;
 };
 
-export const ifSubscribed = async (userId, courseId) => {
-  try {
-    // maybe not best practise to pass user ID as request query
-    // but this is the only format where it works
-    // passing user ID as request body for get request gives error
-    const res = await axios.get(
-      url +
-        "/api/users/subscriptions?user_id=" +
-        userId +
-        "&" +
-        "course_id=" +
-        courseId
-    );
-
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
+export const getAllComponents = async (components) => {
+  const obj = {
+    components,
+  };
+  // Send request to S3 server
+  const res = await axios.post(url + "/api/component/getallcomponents", obj);
+  return res.data;
 };
