89fa01f369416f97637299f74c5e4438720cd963
Bence Szabo <benceszabo657@gmail.com>
2023-10-25 10:43:54 +0200

    Gamified Exercise screen popup merge (#116)
    * updated for testing + began on feature
    
    * Profile settings screen added
    
    Also a bit of cleanup
    
    * settings screen styling
    
    worked on making settings screen look better, not done yet
    
    * settingsButton and styles added
    
    extracted functionality and made a single stylesheet instead of having it hard coded in each button
    
    * Small fix a expo version
    
    * Update StorageService.js
    
    Jeg var før dig Ben
    
    * Update StorageService.js
    
    Cleanup of StorageService.js
    
    * Added environment variables
    
    * Revert "Added environment variables"
    
    This reverts commit 6dee240bad1216e8fc8574da3812c029b5a205ae.
    
    * Revert "Revert "Added environment variables""
    
    This reverts commit c5bd437eb03cb60d1a835ad4ef2cb7bdbcf0492a.
    
    * Commenting
    
    * dotenv added to package.json
    
    * API alignment with backend
    
    * Update deleteAccount.js
    
    * Update ProfileSettings.js
    
    * Revert "Update deleteAccount.js"
    
    This reverts commit bdea176b001f0f9232a2656b4b00af72c6613916.
    
    * Tailwind brokey
    
    * fix
    
    * small adjustment
    
    * fixed view in last commit
    
    * Update app.config.js
    
    * added some styling
    
    * removed styles.css and replaced with tailwind css
    
    * change of ip
    
    * deleteUser working
    
    * Update deleteAccount.js
    
    * added return button + tailwind
    
    * Ben jeg pusher
    
    * modals updated
    
    * small design fix
    
    * adjustments based on sprint review
    
    Made buttons into components and added email validation, removed phone number
    
    * Change email & name in db
    
    * Comments til fixes
    
    * changed text to portugese
    
    * Unit test for useWelcomeScreenLogic
    
    * cleaned up imports
    
    * Clean up and fixes
    
    * translations
    
    * removed scrollview
    
    * luiza feedback adjustments
    
    Username broken down into firstname and lastname, added a colour from figma to tailwindconfig. DB stuff needs fixing
    
    * First & lastname updates seperately
    
    * Back from testing
    
    * Requested changes were made
    
    * Working now
    
    * dependencies updated
    
    * Revert "Gamified edit profile merge"
    
    * Bug fix
    
    Added "import React from 'react' to some files
    
    * small fixes
    
    * Looks wonky, but working
    
    * fix a api
    
    * Update LogOutButton.js
    
    * backButton & header fixed
    
    * Update api.js
    
    * fixed styling
    
    <3
    
    Co-Authored-By: Mafusn <92912046+Mafusn@users.noreply.github.com>
    
    * dotenv and style fixes
    
    <3 2
    
    Co-Authored-By: Mafusn <92912046+Mafusn@users.noreply.github.com>
    
    * Delete deleteAccount.js
    
    * removed unused stuff
    
    * Update ProfileSettings.js
    
    * no courses screen fix + initial route set
    
    * Code Review Changes
    
    * text fixed
    
    * minor changes for pull req
    
    * small fixes
    
    * tests and isFontsLoaded fix
    
    * small fix
    
    * update test config
    
    * chris review
    
    * set disabled and change button text
    
    * animation and overlay fix
    
    * phrases implem.
    
    * quick fix, user_id
    
    * quick again, storageService
    
    * popup and backend connection fix
    
    * styling fix
    
    * ahh
    
    * xp popup animation
    
    * upsi forgor
    
    * moved xp popup to popup, tests brokey:/
    
    * refactor + tests
    
    * moved userinfo and randomPhrase into popup
    
    <3
    
    Co-Authored-By: Louise Steffens <62352035+loui981c@users.noreply.github.com>
    
    * merge fixes
    
    * fixed tests
    
    * launches on correct screen fix + console log removed
    
    * Delete eml/config/keys.js
    
    hihi
    
    ---------
    
    Co-authored-by: Mafusn <Magnus.peetz.holt@gmail.com>
    Co-authored-by: Louise Steffens <foldoysteffenslouise@gmail.com>
    Co-authored-by: Mafusn <92912046+Mafusn@users.noreply.github.com>
    Co-authored-by: Andazu <andersmazen@gmail.com>
    Co-authored-by: Louise Steffens <62352035+loui981c@users.noreply.github.com>

---
+++ eml/test/components/welcomeScreen.test.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useWelcomeScreenLogic } from "../../App"; // Update the import path as needed
+
+// Mock AsyncStorage functions
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+}));
+
+describe("App", () => {
+  it("should set initialRoute to WelcomeStack when hasShownWelcome is false", async () => {
+    AsyncStorage.getItem.mockResolvedValueOnce("false");
+
+    function TestComponent() {
+      const { initialRoute, isLoading } = useWelcomeScreenLogic();
+
+      // Use waitFor to wait for the asynchronous operations to complete
+      waitFor(() => {
+        // Your assertions here
+        expect(initialRoute).toBe("WelcomeStack");
+        expect(isLoading).toBe(false);
+      });
+
+      return null;
+    }
+
+    render(
+      <ErrorBoundary>
+        <TestComponent />
+      </ErrorBoundary>
+    );
+  });
+
+  it("should set initialRoute to LoginStack when hasShownWelcome is true", async () => {
+    AsyncStorage.getItem.mockResolvedValueOnce("true");
+
+    function TestComponent() {
+      const { initialRoute, isLoading } = useWelcomeScreenLogic();
+
+      // Use waitFor to wait for the asynchronous operations to complete
+      waitFor(() => {
+        // Your assertions here
+        expect(initialRoute).toBe("LoginStack");
+        expect(isLoading).toBe(false);
+      });
+
+      return null;
+    }
+
+    render(
+      <ErrorBoundary>
+        <TestComponent />
+      </ErrorBoundary>
+    );
+  });
+
+  it("should handle errors when AsyncStorage operations fail", async () => {
+    AsyncStorage.getItem.mockRejectedValueOnce(new Error("AsyncStorage error"));
+
+    function TestComponent() {
+      const { initialRoute, isLoading } = useWelcomeScreenLogic();
+
+      // Use waitFor to wait for the asynchronous operations to complete
+      waitFor(() => {
+        // Your assertions here for error handling
+        expect(initialRoute).toBe("");
+        expect(isLoading).toBe(true);
+      });
+
+      return null;
+    }
+
+    render(
+      <ErrorBoundary>
+        <TestComponent />
+      </ErrorBoundary>
+    );
+  });
+});
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
