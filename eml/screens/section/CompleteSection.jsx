3591cae43250011ce64ffa916f07bc0ae347529f
Louise Steffens <foldoysteffenslouise@gmail.com>
2023-10-31 14:23:48 +0100

    start of new design

--- eml/screens/section/CompleteSection.jsx
+++ eml/screens/section/CompleteSection.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { View } from 'react-native';
 import Text from '../../components/general/Text';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LottieView from 'lottie-react-native';
 
-export default function CompleteSectionScreen(){
-    return (
-        <SafeAreaView className="bg-bgPrimary h-screen w-screen">
-            <View className="flex flex-col justify-center items-center">
-                <View className="h-4/5 w-4/5 z-10"> 
-                    <LottieView style={{"height":"100%", "width":"100%"}} source={require('../../assets/animations/completeSection.json')} autoPlay loop />
-                </View>
-                <Text className="z-20 -mt-20 text-subheading font-montserrat-bold text-projectWhite p-8 bg-primary rounded-small">
-                    Congrats!
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+export default function CompleteSectionScreen() {
+  // const lottieRef = useRef(null);
+
+	// useEffect(() => {
+  //   const stopAnimation = setTimeout(() => {
+  //     if (lottieRef.current) {
+  //       lottieRef.current.pause();
+  //     }
+  //   }, 1500); // 2 seconds
+
+  //   return () => {
+  //     clearTimeout(stopAnimation);
+  //   };
+  // }, []);
+
+  return (
+    <SafeAreaView className="bg-bgPrimary h-screen w-screen">
+			<View className="h-full w-full z-10 top-0">
+				<LottieView 
+					// ref={lottieRef}
+					style={{"height":"60%", "width":"100%"}} 
+					source={require('../../assets/animations/completeSection.json')} 
+					autoPlay
+				/>
+
+				<View className="flex flex-col justify-center items-center">
+					<View className="h-40 w-72 items-center justify-center">
+						<Text className="z-20 text-center text-3xl font-montserrat-bold text-primary bg-bgPrimary rounded-small">
+						Você está progredindo! Próxima seção, aqui vamos nós!
+						</Text>
+					</View>
+
+					<View className="flex flex-row">
+						<View className="h-20 w-40 bg-yellow">
+
+						</View>
+						<View className="h-20 w-40 bg-success">
+
+						</View>
+					</View>
+				</View>
+			</View>
+
+			
+    </SafeAreaView>
+  );
 };
 
