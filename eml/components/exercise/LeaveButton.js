b8a05050292293ec02f818c8c57049452a8d95fa
sofiiagran <sofiiagran@gmail.com>
2023-10-31 15:05:10 +0100

    Revert "Merge branch 'stage-withLinting' into dev_toStageWithLinting"
    This reverts commit d0d4e079cc71783111d52f5d2a86a394123d8051, reversing
    changes made to bb87ef7d68af8fd9487840395499f7de0eb622a6.

--- eml/components/exercise/LeaveButton.js
+++ eml/components/exercise/LeaveButton.js
@@ -3,36 +3,32 @@ import { StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Icon, Button } from '@rneui/base';
 import PropTypes from 'prop-types';
-import tailwindConfig from '../../tailwind.config';
 
-const LeaveButton = ({ navigationPlace, courseId }) => {
-
-  const projectColors = tailwindConfig.theme.colors;
-
-  LeaveButton.propTypes = {
-    navigationPlace: PropTypes.string.isRequired,
-    courseId: PropTypes.string,
-  };
-  const navigation = useNavigation();
-  return (
-    <Button
-      buttonStyle={styles.buttons}
-      color='invisible'
-      radius='20'
-      size='sm'
-      onPress={() =>
-        navigation.navigate(navigationPlace, { courseId: courseId })
-      }
-      icon={
-        <Icon
-          size={25}
-          name='chevron-left'
-          type='material-community'
-          color={projectColors.projectBlack}
-        />
-      }
-    ></Button>
-  );
+const LeaveButton = ({ navigationPlace, courseID }) => {
+	LeaveButton.propTypes = {
+		navigationPlace: PropTypes.string.isRequired,
+		courseID: PropTypes.string,
+	};
+	const navigation = useNavigation();
+	return (
+		<Button
+			buttonStyle={styles.buttons}
+			color='invisible'
+			radius='20'
+			size='sm'
+			onPress={() =>
+				navigation.navigate(navigationPlace, { courseId: courseID })
+			}
+			icon={
+				<Icon
+					size={25}
+					name='chevron-left'
+					type='material-community'
+					color='#383838'
+				/>
+			}
+		></Button>
+	);
 };
 const styles = StyleSheet.create({
 	buttons: { width: 50, height: 50 },
--- eml/components/exercise/Progressbar.js
+++ eml/components/exercise/Progressbar.js
@@ -3,7 +3,6 @@ import { View, Text } from 'react-native';
 import * as Progress from 'react-native-progress';
 import PropTypes from 'prop-types';
 import { ScreenWidth, ScreenHeight } from '@rneui/base';
-import tailwindConfig from '../../tailwind.config.js';
 
 /**
  * A custom progress bar component.
@@ -20,27 +19,25 @@ const CustomProgressBar = ({ progress, width, height }) => {
 		height: PropTypes.number.isRequired,
 	};
 
-  const projectColors = tailwindConfig.theme.colors;
+	// Insure progress is between 0 and 100
+	progress = Math.min(100, Math.max(0, progress));
 
-  // Insure progress is between 0 and 100
-  progress = Math.min(100, Math.max(0, progress));
-
-  return (
-    <View className='flex-row items-center justify-around'>
-      <Progress.Bar
-        progress={progress / 100}
-        width={ScreenWidth * (width / 100)}
-        height={ScreenHeight * (height / 100)}
-        color= {projectColors.progressBar}
-        unfilledColor={projectColors.progressBarUnFilled}
-        borderWidth={0}
-        borderRadius={8}
-      ></Progress.Bar>
-      <Text className='px-5 text-center font-montserrat-bold text-caption-medium text-projectBlack'>
-        {progress}%
-      </Text>
-    </View>
-  );
+	return (
+		<View className='flex-row items-center justify-around'>
+			<Progress.Bar
+				progress={progress / 100}
+				width={ScreenWidth * (width / 100)}
+				height={ScreenHeight * (height / 100)}
+				color='rgba(94, 204, 233, 1)'
+				unfilledColor='rgba(228, 242, 245, 1)'
+				borderWidth={0}
+				borderRadius={8}
+			></Progress.Bar>
+			<Text className='px-5 text-center font-montserrat-bold text-caption-medium text-projectBlack'>
+				{progress}%
+			</Text>
+		</View>
+	);
 };
 
 export default CustomProgressBar;
