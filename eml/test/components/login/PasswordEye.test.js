db2267cd7f25bf5d8dddd9e3f50d0e998c30ea47
BenjaminVilladsen <benmicvil@gmail.com>
2023-11-01 12:16:47 +0100

    newer tests

---
+++ eml/test/components/login/PasswordEye.test.js
@@ -0,0 +1,30 @@
+import renderer from "react-test-renderer";
+import React from "react";
+import PasswordEye from "../../../components/login/PasswordEye";
+
+const mockFunction = jest.fn();
+
+test("Ensure that the field is rendered correctly without props", async () => {
+  let passwordEye;
+  await renderer.act(() => {
+    passwordEye = renderer.create(<PasswordEye />);
+  })
+  let tree = passwordEye.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Ensure that the field props are rendered correctly with props", async () => {
+  let passwordEye;
+  await renderer.act(() => {
+    passwordEye = renderer.create(<PasswordEye 
+      name="eye" 
+      size={24} 
+      color="gray"
+      onPress={mockFunction}
+    />);
+  })
+  expect(passwordEye.root.props.name).toBe("eye");
+  expect(passwordEye.root.props.size).toBe(24);
+  expect(passwordEye.root.props.color).toBe("gray");
+  expect(passwordEye.root.props.onPress).toBe(mockFunction);
+});
