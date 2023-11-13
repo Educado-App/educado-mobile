16bddac8995066be4c97abb81cc1f9fd4c6106f6
ITPini <marc6571us@gmail.com>
2023-11-13 23:12:17 +0100

    Adhered OfflineBanner.jsx to ESLint
    Applied ESLint
    ESLint: Unnecessary try/catch wrapper (no-useless-catch)

--- eml/components/general/OfflineBanner.jsx
+++ eml/components/general/OfflineBanner.jsx
@@ -1,50 +1,46 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Animated, Text } from 'react-native';
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import {checkIfOnline} from "../../services/StorageService";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { checkIfOnline } from '../../services/StorageService';
 
 export default function OfflineBanner() {
-    const [isBackendReachable, setIsBackendReachable] = useState(false);
-    const translateY = useRef(new Animated.Value(-100)).current;
+  const [isBackendReachable, setIsBackendReachable] = useState(false);
+  const translateY = useRef(new Animated.Value(-100)).current;
 
-    const checkBackendConnection = async () => {
-        try {
-            setIsBackendReachable(await checkIfOnline());
-        } catch (error) {
-            throw error;
-        }
-    };
+  const checkBackendConnection = async () => {
+    setIsBackendReachable(await checkIfOnline());
+  };
 
-    useEffect(() => {
-        // Check once on mount
-        checkBackendConnection();
+  useEffect(() => {
+    // Check once on mount
+    checkBackendConnection();
 
-        const intervalId = setInterval(checkBackendConnection, 10000);
+    const intervalId = setInterval(checkBackendConnection, 10000);
 
-        // Clear interval on component unmount
-        return () => clearInterval(intervalId);
-    }, []);
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
-    useEffect(() => {
-        Animated.timing(translateY, {
-            toValue: isBackendReachable ? -100 : 0, // Slide in or out
-            duration: 300,
-            useNativeDriver: true,
-        }).start();
-    }, [isBackendReachable, translateY]);
+  useEffect(() => {
+    Animated.timing(translateY, {
+      toValue: isBackendReachable ? -100 : 0, // Slide in or out
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [isBackendReachable, translateY]);
 
-    return (
-        <Animated.View style={[{
-            transform: [{translateY}],
-            position: 'absolute',
-            top: 0,
-            width: '100%',
-            zIndex: 10,
-        }]} className='bg-error flex-row pb-2 justify-center items-end h-[10%]'>
-            <MaterialCommunityIcons name={'wifi-off'} color='white' size={20}/>
-            <Text className={"text-projectWhite px-2"}>
+  return (
+    <Animated.View style={[{
+      transform: [{translateY}],
+      position: 'absolute',
+      top: 0,
+      width: '100%',
+      zIndex: 10,
+    }]} className='bg-error flex-row pb-2 justify-center items-end h-[10%]'>
+      <MaterialCommunityIcons name={'wifi-off'} color='white' size={20}/>
+      <Text className={'text-projectWhite px-2'}>
                 Sem conexão com a internet!
-            </Text>
-        </Animated.View>
-    );
+      </Text>
+    </Animated.View>
+  );
 }
