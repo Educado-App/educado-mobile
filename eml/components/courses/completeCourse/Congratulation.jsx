fb90c78af7e456ecd736e84855bb8be8ac8d9018
Louise Steffens <foldoysteffenslouise@gmail.com>
2023-11-17 19:34:17 +0100

    getting name from storage

--- eml/components/courses/completeCourse/Congratulation.jsx
+++ eml/components/courses/completeCourse/Congratulation.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import Text from '../../general/Text';
 import LottieView from 'lottie-react-native';
+import { getUserInfo } from '../../../services/StorageService';
 
 export default function Congratulation() {
+  const [name, setName] = useState('');
+
+  const getName = async () => {
+    const userInfo = await getUserInfo();
+    setName(userInfo.firstName);
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+
   return (
     <View className="flex w-full h-full justify-start items-center">
       <Text className="text-center font-sans-bold text-3xl text-primary p-5">Excelente trabalho, você terminou!</Text>
@@ -21,7 +33,7 @@ export default function Congratulation() {
 
       <View>
         <Text className="text-center text-base text-projectBlack px-5 mt-12">
-          Bom trabalho, NAME! Você pode ver suas estatísticas, placar educado e certificação antes de continuar.
+          Bom trabalho, {name}! Você pode ver suas estatísticas, placar educado e certificação antes de continuar.
         </Text>
       </View>
     </View>
