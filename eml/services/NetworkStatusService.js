ea848b34bcab6fa03b216bae59503b4d2d425f75
ITPini <marc6571us@gmail.com>
2023-11-21 14:48:08 +0100

    Added JSDoc and adhered to ESLint standard

--- eml/services/NetworkStatusService.js
+++ eml/services/NetworkStatusService.js
@@ -1,10 +1,14 @@
-import NetInfo from "@react-native-community/netinfo";
+import NetInfo from '@react-native-community/netinfo';
 
+/**
+ * Singleton class for monitoring network status.
+ * Utilizes NetInfo from '@react-native-community/netinfo' to listen for network changes.
+ */
 export class NetworkStatusService {
   static #instance;
   #isOnline = false;
   #observers = [];
-  #simulateOffline = false;
+  #simulateOffline = false; // Flag to simulate offline status for testing purposes. TODO: Implement in a future .env file.
 
   constructor() {
     if (NetworkStatusService.#instance) {
@@ -14,6 +18,10 @@ export class NetworkStatusService {
     NetworkStatusService.#instance = this;
   }
 
+  /**
+   * Provides the singleton instance of the NetworkStatusService.
+   * @returns {NetworkStatusService} The singleton instance.
+   */
   static getInstance() {
     if (!this.#instance) {
       this.#instance = new NetworkStatusService();
@@ -21,21 +29,37 @@ export class NetworkStatusService {
     return this.#instance;
   }
 
+  /**
+   * Adds an observer to the list. Observers are notified of network status changes.
+   * @param {Object} observer - The observer object that should have an 'update' method.
+   * @returns {boolean} The current network status.
+   */
   addObserver(observer) {
     this.#observers.push(observer);
     return this.#isOnline;
   }
 
+  /**
+   * Removes an observer from the list.
+   * @param {Object} observer - The observer to remove.
+   */
   removeObserver(observer) {
     this.#observers = this.#observers.filter(obs => obs !== observer);
   }
 
+  /**
+   * Initializes the NetInfo event listener to monitor network changes.
+   */
   #initializeNetworkInfo() {
     NetInfo.addEventListener(state => {
       this.#updateNetworkStatus(state.isConnected);
     });
   }
 
+  /**
+   * Updates the network status and notifies observers if there's a change.
+   * @param {boolean} isOnline - The current network status.
+   */
   #updateNetworkStatus(isOnline) {
     // Adjust network status based on simulateOffline
     let networkStatus = this.#simulateOffline ? false : isOnline;
@@ -46,6 +70,9 @@ export class NetworkStatusService {
     }
   }
 
+  /**
+   * Notifies all observers about the current network status.
+   */
   #notifyObservers() {
     this.#observers.forEach(observer => observer.update(this.#isOnline));
   }
