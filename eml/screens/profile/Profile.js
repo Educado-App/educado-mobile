41e84f68618b9d97620f1752647ea5b2de0d3921
Anders Mazen Youssef <andersmazen@gmail.com>
2023-11-22 15:48:19 +0100

    profile level update
    added:
    - images
    - progressbar with remade label parameter
    - colors to tailwind for profile green purple and blue colors
    - made it acording to figma

--- eml/screens/profile/Profile.js
+++ eml/screens/profile/Profile.js
@@ -3,12 +3,16 @@ import {
   View,
   SafeAreaView,
   ScrollView,
+  Text,
+  Image,
 } from 'react-native';
 import LogOutButton from '../../components/profile/LogOutButton';
 import ProfileNavigationButton from '../../components/profile/ProfileNavigationButton.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import UserInfo from '../../components/profile/UserInfo';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from '@rneui/themed';
+import CustomProgressBar from '../../components/exercise/Progressbar';
 
 const USER_INFO = '@userInfo';
 
@@ -56,6 +60,33 @@ export default function ProfileComponent() {
       <ScrollView className='flex flex-col'>
         <View className="flex-1 justify-start pt-[20%] h-screen">
           <UserInfo firstName={firstName} lastName={lastName} email={email} points={points}></UserInfo>
+          <View className='m-6 rounded-medium border-projectGray border-2'>
+            <View className='flex flex-row justify-between items-center p-3 w-full'>
+              <View className='flex flex-col bg-badgesGreen justify-center items-center py-2 rounded-medium w-[32%]'>
+                <Image
+                  source={require('../../assets/images/profileFlame.png')}
+                />
+                <Text className='text-projectWhite font-sans-bold pt-3'>1 dia seguido</Text>
+              </View>
+              <View className='flex flex-col bg-badgesPurple justify-center items-center py-2 rounded-medium w-[32%] mx-2'>
+                <Image
+                  source={require('../../assets/images/profileCoin.png')}
+                />
+                <Text className='text-projectWhite font-sans-bold pt-3'>87 pontos</Text>
+              </View>
+              <View className='flex flex-col bg-badgesBlue justify-center items-center py-2 rounded-medium w-[32%]'>
+                <Image
+                  source={require('../../assets/images/profileLightning.png')}
+                  className=''
+                />
+                <Text className='text-projectWhite font-sans-bold pt-3'>3º posição</Text>
+              </View>
+            </View>
+            <View className='flex flex-row justify-between p-4 border-projectGray border-t-2'>
+              <Text className='font-sans-bold text-primary'>Nível 1</Text>
+              <CustomProgressBar progress={50} width={65} height={1} displayLabel={false}></CustomProgressBar>
+            </View>
+          </View>
           <ProfileNavigationButton label='Editar perfil' testId={'editProfileNav'} onPress={() => navigation.navigate('EditProfile')}></ProfileNavigationButton>
           <ProfileNavigationButton label='Certificados'></ProfileNavigationButton>
           <ProfileNavigationButton label='Download'></ProfileNavigationButton>
