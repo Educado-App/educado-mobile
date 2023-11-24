4d02c87c8c481cd439549421088761853e8e85d8
MortenMunk <mortenmunk97@gmail.com>
2023-11-24 13:33:51 +0100

    fix: removed dropdown from card

--- eml/components/certificate/CertificateCard.jsx
+++ eml/components/certificate/CertificateCard.jsx
@@ -15,25 +15,13 @@ import CertificateBtn from './CertificateBtn';
  * @returns {JSX.Element|null} - Returns a JSX element.
  */
 export default function CertificateCard({ certificate, previewOnPress }) {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-
   return (
-    <Pressable
-      className="bg-projectWhite rounded-lg shadow-2xl mb-4 mx-4 p-6 overflow-hidden"
-      onPress={() => setIsCollapsed(!isCollapsed)}
-    >
+    <View className="bg-projectWhite rounded-lg shadow-2xl mb-4 mx-4 p-6 overflow-hidden">
       <View className="flex-col items-center">
         <View className="flex-row justify-between w-full items-center">
           <Text className="text-black font-medium text-lg">{certificate.courseName}</Text>
-          <MaterialCommunityIcons
-            name={isCollapsed ? 'chevron-down' : 'chevron-up'}
-            size={25}
-            color="gray"
-          />
         </View>
-
         <View className="h-1 border-b-[1px] w-full border-gray opacity-50 pt-2"></View>
-
         <View className="w-full h-[0.5] bg-gray-500 opacity-50 pt-2" />
         <View className="flex-row justify-between w-full items-start">
           <View className="flex-col items-start justify-between">
@@ -55,16 +43,7 @@ export default function CertificateCard({ certificate, previewOnPress }) {
           </View>
         </View>
       </View>
-
-
-      <Collapsible className="w-full" collapsed={isCollapsed}>
-        <View className="py-1 flex-row items-center justify-between px-1">
-          <CertificateBtn buttonText="Preview" onPress={previewOnPress} />
-          <CertificateBtn buttonText="Download" />
-        </View>
-
-      </Collapsible>
-    </Pressable>
+    </View>
   );
 }
 
