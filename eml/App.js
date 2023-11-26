efbbc99b3a9878d9935e0f93b73981ceb0929371
Bence Szabo <benceszabo657@gmail.com>
2023-11-26 19:16:46 +0100

    duplicate CertificateStack fix

--- eml/App.js
+++ eml/App.js
@@ -232,13 +232,6 @@ export default function App() {
                   headerShown: false,
                 }}
               />
-              <Stack.Screen
-                name="CertificateStack"
-                component={CertificateStack}
-                options={{
-                  headerShown: false,
-                }}
-              />
             </Stack.Navigator>
           </NavigationContainer>
         </ApplicationProvider>
