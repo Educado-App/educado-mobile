4bde5d85f30ab6a9ee90fb0aeb0b0a26515effa2
sofiiagran <sofiiagran@gmail.com>
2023-11-28 13:23:07 +0100

    update routes

--- eml/api/userApi.js
+++ eml/api/userApi.js
@@ -8,7 +8,7 @@ const local = 'http://localhost:8888';
 const digitalOcean = 'http://207.154.213.68:8888';
 */ 
 
-const url = 'https://educado-backend-staging-x7rgvjso4a-ew.a.run.app/'; // Change this to your LOCAL IP address when testing.
+const url = 'http://172.30.211.221:8888/'; // Change this to your LOCAL IP address when testing.
 
 /**
  * This is the client that will be used to make requests to the backend.
@@ -39,6 +39,7 @@ export const registerUser = async (obj) => {
     console.log('User successfully registered');
     return res.data;
   } catch (e) {
+    console.log(e.message);
     if (e?.response?.data != null) {
       throw e.response.data;
     } else {
@@ -124,13 +125,16 @@ export const updateUserPassword = async (user_id, oldPassword, newPassword, toke
     }
   }
 };
-
-export const completeComponent = async (user_id, comp, isComplete, points, token) => {
-  try{
-    const res = await client.patch('/api/students/' + user_id + '/complete', { comp: comp, isComplete: isComplete, points: points }, {
+export const completeComponent = async (userId, componentId, isComplete, points, token) => {
+  try {
+    const res = await client.patch(`/api/students/${userId}/complete`, {
+      componentId: componentId,
+      isComplete: isComplete,
+      points: points,
+    }, {
       headers: {
         'Content-Type': 'application/json',
-        'token': token, // Include the token in the headers
+        'Authorization': `Bearer ${token}`, // Using a Bearer token for authentication
       },
     });
 
@@ -144,50 +148,34 @@ export const completeComponent = async (user_id, comp, isComplete, points, token
   }
 };
 
+
 export const getStudentInfo = async (user_Id) => {
   try {
     const res = await client.get('/api/students/' + user_Id + '/info');
     return res.data;
-  } catch (err) {
-    return err.message;
+  } catch (e) {
+    if (e?.response?.data != null) {
+      throw e.response.data;
+    } else {
+      throw e;
+    }
   }
 };
 
-export const enrollInCourse = async (user_Id, course_Id) => {
+
+
+export const addCourseToStudent = async (studentId, courseId, token) => {
   try {
-    // When user enrolls in a course it sends the course id to the database,
-    // and then stores the course and completion status in the user document.
-    const res = await client.post('/api/eml/' + user_Id + '/enroll/' + course_Id);
-
-    // First time user enrolls in course
-    if (!res.data.course) {
-      return res.data;
-    }
-
-    // if the course already exists, return the completion status of the course, sections, and exercises
-    return {
-      courseCompletion: res.data.course.isComplete,
-      sectionCompletion: res.data.course.sections.map(
-        (section) => section.isComplete
-      ),
-      exerciseCompletion: res.data.course.sections.map((section) =>
-        section.exercises.map((exercise) => exercise.isComplete)
-      ),
-    };
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-export const updateCourseStatus = async (user_id, course_id) => {
-  try{
-    // When user completes course it should update the user document from
-    // isComplete: false, to isComplete: true for that course
-    const res = await client.put('/api/eml/' + user_id + '/updateCourse/' + course_id);
+    const res = await client.post(
+      `/api/students/${studentId}/courses/${courseId}/enroll`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      }
+    );
     return res.data;
   } catch (e) {
     if (e?.response?.data != null) {
@@ -198,55 +186,6 @@ export const updateCourseStatus = async (user_id, course_id) => {
   }
 };
 
-export const updateSectionStatus = async (user_id, course_id, section_id) => {
-  try{
-    // When user completes section it should update the user document from
-    // isComplete: false, to isComplete: true for that section
-    const res = await client.put('/api/eml/' + user_id + '/updateSection/' + course_id + '/' + section_id);
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-export const updateExerciseStatus = async (
-  user_id,
-  course_id,
-  section_id,
-  exercise_id
-) => {
-  try{
-    // When user completes an exercise it should update the user document from
-    // isComplete: false, to isComplete: true for that exercise
-    const res = await client.put('/api/eml/' + user_id + '/updateExercise/'
-      + course_id + '/' + section_id + '/' + exercise_id);
-    return res.data;
-  } catch (e) {
-    if (e?.response?.data != null) {
-      throw e.response.data;
-    } else {
-      throw e;
-    }
-  }
-};
-
-export const addCourseToStudent = async (user_Id, course_Id, token) => {
-  const res = await client.patch('/api/students/' + user_Id + '/courses/' + course_Id + '/add',
-    {},
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'token': token, // Include the token in the headers
-      },
-    }
-  );
-
-  return res.data;
-};
 
 /**
  * Function to send mail to user with code to reset password
