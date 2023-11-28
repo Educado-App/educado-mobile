9847c6426a9fd718f9d3d22aec5df6fd6ff80717
sofiiagran <sofiiagran@gmail.com>
2023-11-28 15:27:36 +0100

    hotfix part 2

--- eml/services/StorageService.js
+++ eml/services/StorageService.js
@@ -216,6 +216,7 @@ export const refreshSectionList = async (sectionList) => {
           sectionId: section._id,
           parentCourseId: section.parentCourse,
           description: section.description,
+          components: section.components,
           total: section.components.length,
         });
       }
