35cc9d460405db524d858b77321d0d310305a5b8
sofiiagran <sofiiagran@gmail.com>
2023-11-29 09:52:46 +0100

    test to trigger github actions

--- eml/api/api.js
+++ eml/api/api.js
@@ -6,7 +6,7 @@ const url = 'https://educado-backend-staging-x7rgvjso4a-ew.a.run.app/';// Change
 
 const certificateUrl = 'http://172.30.245.212:8080';
 
-/* Commented out for avoiding linting errors
+/* Commented out for avoiding linting errors :))
  * TODO: move IP address to .env file !!!
 const testUrl = 'http://localhost:8888';
 const testExpo = 'http://172.30.211.57:8888'; 
