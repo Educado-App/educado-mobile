598c81ef0156fa61f0b55f575e5b95bff252f781
Anders <andersraae01@gmail.com>
2024-09-17 15:57:28 +0200

    unlinted

--- eml/.eslintrc.js
+++ eml/.eslintrc.js
@@ -1,51 +1,52 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  overrides: [
-    {
-      files: [
-        '**/*.js', // Include .js files
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'module',
-      },
-    },
-    {
-      files: [
-        '**/*.jsx', // Include .jsx files
-      ],
-      parser: '@babel/eslint-parser', // Specify the parser
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-          js: true,
-        },
-        sourceType: 'module',
-      },
-    },
-  ],
-  plugins: ['react'],
-  ignorePatterns: [
-    '**/*.test.js',
-    '**/*.test.jsX',
-    '**/*.spec.js',
-    '**/*.spec.jsx',
-    '**/*.config.js',
-    'jestSetup.js',
-  ],
-  rules: {
-    quotes: ['error', 'single'], // Specify single quotes
-    semi: ['error', 'always'], // Require semicolons
-    'no-unsafe-finally': 'off', // Disable no-unsafe-finally rule
-  },
-  settings: {
-    react: {
-      version: '16.13.1',
-    },
-  },
+	'env': {
+		'browser': true,
+		'es2022': true,
+		'node': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+		{
+			'files': [
+				'**/*.js', // Include .js files
+				'.eslintrc.{js,cjs}',
+			],
+			'parserOptions': {
+				'sourceType': 'module'
+			}
+		},
+		{
+			'files': [
+				'**/*.jsx', // Include .jsx files
+			],
+			'parser': '@babel/eslint-parser', // Specify the parser
+			'parserOptions': {
+				'ecmaFeatures': {
+					'jsx': true,
+					'js': true,
+				},
+				'sourceType': 'module'
+			}
+		}
+	],
+	'plugins': [
+		'react'
+	],
+	'ignorePatterns': [
+		'**/*.test.js', '**/*.test.jsX', '**/*.spec.js', '**/*.spec.jsx', '**/*.config.js', 'jestSetup.js',
+	],
+	'rules': {
+		'indent': ['error', 'tab'], // Use tabs for indentation, it is set to two spaces
+		'quotes': ['error', 'single'], // Specify single quotes
+		'semi': ['error', 'always'], // Require semicolons
+		'no-unsafe-finally': 'off', // Disable no-unsafe-finally rule
+	},
+	'settings': {
+		'react': {
+			'version': '16.13.1'
+		}
+	}
 };
