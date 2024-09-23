986ad833f7c27b269a46db89bba1d4a3b2896b34
MBryder <mbryde22@student.aau.dk>
2024-09-23 11:18:51 +0200

    Update API

--- eml/api/api.js
+++ eml/api/api.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const timeoutInMs = 1200;
 
-const url = '172.20.10.3';// Change this to your LOCAL IP address when testing.
+const url = 'https://educado-backend-staging-x7rgvjso4a-ew.a.run.app/';// Change this to your LOCAL IP address when testing.
 
 const certificateUrl = 'https://educado-certificate-service-staging-x7rgvjso4a-ew.a.run.app/';
-
 
 /* Commented out for avoiding linting errors :))
  * TODO: move IP address to .env file !!!
