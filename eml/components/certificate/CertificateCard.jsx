b519fa67157cd34ce86a1cd1c6ded02ed0c6a5d6
Lasse Drongesen <Lassemgp@gmail.com>
2024-10-15 11:40:40 +0200

    smol change

--- eml/components/certificate/CertificateCard.jsx
+++ eml/components/certificate/CertificateCard.jsx
@@ -32,14 +32,12 @@ export default function CertificateCard({ certificate }) {
 				dateOfCompletion={Utility.formatDate(certificate.dateOfCompletion)}
 				creatorName={certificate.courseCreator}
 			/>
-			<View className="absolute bottom-0 left-0 right-0 h-1/2 bg-projectWhite rounded-lg mx-4 px-2">
-					<View className="flex-col items-end mx-4 relative">
-						<View className="flex-row justify-between w-full items-center">
-							<Text className="text-black font-medium text-lg">{certificate.courseName}</Text>
-						</View>
-						<View className="h-1 border-b-[1px] w-full border-gray opacity-50 pt-2"></View>
+			<View className="absolute bottom-0 left-0 right-0 h-1/2 bg-projectWhite rounded-lg mx-4 px-2 opacity-95">
+					<View className="flex-col mx-4 relative">
+						<Text className="text-black font-medium text-lg mt-2">{certificate.courseName}</Text>
+						<View className="h-1 border-b-[1px] w-full border-gray opacity-20 pt-1"></View>
 						<View className="w-full h-[0.5] bg-gray-500 opacity-50 pt-2" />
-						<View className="flex-row justify-between w-full items-start mb-6">
+						<View className="flex-row justify-between w-full items-start">
 							<View className="flex-col items-start justify-between">
 								<View className="flex-row items-center justify-start pb-2 flex-wrap">
 									<CardLabel
@@ -58,18 +56,17 @@ export default function CertificateCard({ certificate }) {
 								</View>
 							</View>
 						</View>
-						<TouchableOpacity className="flex items-end absolute bottom-0 right-0" onPress={handleVisualizarClick}>
-							<View className="flex flex-row items-center justify-center">
+					</View>
+					<TouchableOpacity className="flex items-center absolute bottom-0 right-0 m-6" onPress={handleVisualizarClick}>
+							<View className="flex flex-row items-center justify-center mt-2">
 								<Text className="text-primary_custom font-bold mr-1">visualizar</Text>
-								<MaterialCommunityIcons name={'eye'} size={13} color={"#166276"} />
+								<MaterialCommunityIcons name={'eye'} size={13} color={"#166276"}/>
 							</View>
 							<View className="border-b-[1px] w-[90px] border-primary_custom pt-[2px]"></View>
-						</TouchableOpacity>
-					</View>
+					</TouchableOpacity>
 				</View>
+				<Popup visible={popupVisible} onClose={handleClosePopup} />
 			</View>
-			<Popup visible={popupVisible} onClose={handleClosePopup} />
-		</View>
 	);
 }
 
